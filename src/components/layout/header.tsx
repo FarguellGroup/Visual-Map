8832cf9b2c430eea33be462dbf63707cb8e3e88a
afsh4ck <132138425+afsh4ck@users.noreply.github.com>
2025-10-20 15:37:20 +0200

    Update header.tsx

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -28,10 +28,6 @@ export default function AppHeader() {
     <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background/95 px-4 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="flex items-center gap-2">
         {showSidebarTrigger && <SidebarTrigger />}
-        <Link href="/" className="flex items-center gap-2">
-            <VmLogo className="h-6 w-6" />
-            <h1 className="text-lg md:text-xl font-bold tracking-tight">{t('title')}</h1>
-        </Link>
       </div>
 
       <div className="flex flex-1 items-center justify-end space-x-2">
