1baded9ef64f2d9d935e4b3b75cacf5981f6ee87
afsh4ck <132138425+afsh4ck@users.noreply.github.com>
2025-10-21 11:32:38 +0200

    Update sidebar-provider.tsx

--- src/components/layout/sidebar-provider.tsx
+++ src/components/layout/sidebar-provider.tsx
@@ -1,12 +1,18 @@
-
 'use client';
 
 import { SidebarProvider } from '@/components/ui/sidebar';
+import { usePathname } from '@/navigation';
+import { useScanStore } from '@/store/use-scan-store';
 
 export function ClientSidebarProvider({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <SidebarProvider>{children}</SidebarProvider>;
+  const pathname = usePathname();
+  const { scanResult } = useScanStore();
+  const showSidebar = scanResult || pathname.includes('/details');
+  const defaultOpen = !!showSidebar;
+  
+  return <SidebarProvider key={String(defaultOpen)} defaultOpen={defaultOpen}>{children}</SidebarProvider>;
 }
