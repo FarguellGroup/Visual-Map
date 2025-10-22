69fb3253af9bf28defbd3b4dc8f4f443f1a228c6
afsh4ck <132138425+afsh4ck@users.noreply.github.com>
2025-10-22 11:13:49 +0200

    Update i18n.ts

--- i18n.ts
+++ i18n.ts
@@ -1,6 +1,23 @@
 import {getRequestConfig} from 'next-intl/server';
+import {locales} from './src/navigation';
+import {notFound} from 'next/navigation';
+
+// Statically import messages
+import enMessages from './messages/en.json';
+import esMessages from './messages/es.json';
+
+const messages = {
+  en: enMessages,
+  es: esMessages,
+};
  
-export default getRequestConfig(async ({locale}) => ({
-  locale,
-  messages: (await import(`./messages/${locale}.json`)).default
-}));
+export default getRequestConfig(async ({locale}) => {
+  // Validate that the incoming `locale` parameter is valid
+  if (!locales.includes(locale as any)) {
+    notFound();
+  }
+
+  return {
+    messages: messages[locale as keyof typeof messages]
+  };
+});
