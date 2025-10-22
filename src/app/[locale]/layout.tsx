28a12a4a431a7f3a775d3dfca12e7a7747412e30
afsh4ck <132138425+afsh4ck@users.noreply.github.com>
2025-10-22 11:16:55 +0200

    Update layout.tsx

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
-import AppFooter from '@/components/layout/footer';
-import AppHeader from '@/components/layout/header';
 import { unstable_setRequestLocale } from 'next-intl/server';
 import { ClientSidebarProvider } from '@/components/layout/sidebar-provider';
-import ApiErrorToast from '@/components/api-error-toast';
 import MainLayout from '@/components/layout/main-layout';
 
 export default function LocaleLayout({
