8c986e4419fd9d93ca97827d4cb09bcb4eb6feca
afsh4ck <132138425+afsh4ck@users.noreply.github.com>
2025-10-23 15:32:30 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,7 +5,6 @@ const withNextIntl = createNextIntlPlugin('./src/i18n.ts');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   output: 'standalone',
   devIndicators: {
     buildActivity: false,
